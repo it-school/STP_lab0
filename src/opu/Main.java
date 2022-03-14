40c683c614d55861726eccc096a5abfa201df3fd
Viktor Speranskyy <vencedor@ukr.net>
2022-03-14 10:43:56 +0200

    Added 2 more text lines

--- src/opu/Main.java
+++ src/opu/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Lesson 0");
         System.out.println("New line");
         System.out.println("Lesson 0");
+        System.out.println("New line");
+        System.out.println("Lesson 0");
     }
 }
